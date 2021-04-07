1e8e3f0a0fe18c3f95c16eca108efe166ab127fd
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2021-04-06 21:50:22 -0300

    init project

---
+++ controllers/atendimentos.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Rota atendimentos'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Atendimentos Post')
+    })
+}
