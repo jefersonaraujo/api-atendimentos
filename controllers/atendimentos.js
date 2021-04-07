de794ebb23428fa3ac2c71897640a2196d3dfb9e
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-04-07 08:20:59 -0300

    models

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota atendimentos'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+
         res.send('Atendimentos Post')
         
     })
