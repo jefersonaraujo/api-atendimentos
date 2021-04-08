39c41b95358097a51c09c37a7b1380e6f38c2a2d
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-04-08 14:58:13 -0300

    Update e List

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,13 +1,26 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Rota atendimentos'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
 
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id, res)
+    })
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
         Atendimento.adiciona(atendimento, res)
         
         
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+    
+        Atendimento.altera(id, valores, res)
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,18 +5,88 @@ class Atendimentos {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length  >=5
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro)       
-            } else {
-                res.status(201).json(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        }
+        else {
             
-        })
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro)       
+                } else {
+                    res.status(201).json(resultados)
+                }
+                
+            })
+        }
+
     }
 
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else{
+                res.status(200).json(resultados)
+            }
+        })
+
+    }
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+    
+        conexao.query(sql, (erro, restultados) => { 
+            const atendimento = restultados[0]
+            if(erro) { 
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+    
+    })
+}  
+
+    altera(id, valores, res) {
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => { 
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+} 
+
+//    
 }
 module.exports = new  Atendimentos
