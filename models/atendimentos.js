8578b78f86632809dc7240512ae714fcb6387036
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-04-08 15:13:21 -0300

    delete

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -87,6 +87,17 @@ class Atendimentos {
         })
 } 
 
+deleta(id, res) {
+    const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+    conexao.query(sql, id, (erro, resultados) => {
+        if(erro) {
+            res.status(400).json(erro)
+        } else {
+            res.status(200).json({resultados})
+        }
+    })
+}
 //    
 }
 module.exports = new  Atendimentos
