61051753eef298cad92eaab9c02e7b82bf3ed346
Jeferson Araujo <jefersonaraujo95@gmail.com>
2021-04-16 11:11:23 -0300

    adicionando agent apm

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
+const apm = require ('elastic-apm-node'). start ({ 
+    serviceName: 'api-atendimentos', 
+    serverUrl: 'http://192.168.1.141:8200', 
+  })
+
+
 conexao.connect(erro => {
     if(erro){
         console.log(erro)
@@ -11,6 +17,8 @@ conexao.connect(erro => {
         Tabelas.init(conexao)
         const  app = customExpress()
         app.listen(3000, () => console.log('Servidor rodando na portal 3000'))
+
+        
     }
 })
 
